0118fdcfee1e663773dabe616a33d7f3b4cceee5
Quang Cat <catvinhquang@gmail.com>
2020-11-26 19:07:03 +0700

    Push notification when update gold price

--- app/src/main/java/com/catvinhquang/exchangerate/ExchangeRateApplication.kt
+++ app/src/main/java/com/catvinhquang/exchangerate/ExchangeRateApplication.kt
@@ -1,13 +1,8 @@
 package com.catvinhquang.exchangerate
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequest
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import com.catvinhquang.exchangerate.data.DataProvider
 import com.catvinhquang.exchangerate.workers.GetGoldPriceWorker
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by QuangCV on 13-Aug-2020
@@ -18,24 +13,7 @@ class ExchangeRateApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DataProvider.init(this)
-        initGetGoldPriceWorker()
-    }
-
-    private fun initGetGoldPriceWorker() {
-        val request = PeriodicWorkRequestBuilder<GetGoldPriceWorker>(
-            PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS,
-            TimeUnit.MILLISECONDS
-        ).setInitialDelay(
-            PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS,
-            TimeUnit.MILLISECONDS
-        ).build()
-
-        WorkManager.getInstance(this)
-            .enqueueUniquePeriodicWork(
-                "ExchangeRate-GetGoldPrice",
-                ExistingPeriodicWorkPolicy.KEEP,
-                request
-            )
+        GetGoldPriceWorker.schedule(this)
     }
 
 }
